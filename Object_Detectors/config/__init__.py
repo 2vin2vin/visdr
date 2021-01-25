d92ab5aee22007d8bb516e8a58bb14ec12f6497c
2vin2vin <sohithvulpie@gmail.com>
2021-01-26 00:31:17 +0530

    config

---
+++ Object_Detectors/config/__init__.py
@@ -0,0 +1,7 @@
+#  Copyright (c) 2020.
+#  Airodyn Systems Private Limited
+
+# ______________________________________
+#   File Name:          
+#   Author:             Aviral Sharma
+# ______________________________________
