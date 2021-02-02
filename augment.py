f62a8acea1cc7410d08dfaaccf9c7e0d9ed44956
2vin2vin <sohithvulpie@gmail.com>
2021-02-02 20:13:33 +0530

    correct

---
+++ augment.py
@@ -0,0 +1,288 @@
+import cv2
+import numpy as np
+import pandas as pd
+import glob,os,random,imutils,math
+
+
+def up_to_down(img,random_value,b):
+    bore=1#vertical flip
+    '''if random number >0.5 then flip'''
+    if random_value>0.5:
+        a=img.copy()
+        img3=a[::-1,:,:]
+        img3=img3.copy()
+        x,y,w,h=b
+        w,h=w-x,h-y
+        y=img.shape[0]-y
+        coord=[x,y,x+w,y-h]
+        return img3,coord
+    else:
+        return img,b
+
+def left_to_right(img,random_value,b):
+    bore=1#horizontal flip
+    '''if random number >0.5 then flip'''
+    if random_value>0.5:
+        a=img.copy()
+        img2 = a[:,::-1,:]
+        img3=img2.copy()
+        x,y,w,h=b
+        w,h=w-x,h-y
+        x=img.shape[1]-x
+        coord=[x-w,y,x,y+h]
+        return img3,coord
+    else:
+        return img,b
+
+def left_or_right(img,translate,random_value,b):
+    #translate
+    '''if random number >0.5 then left or else right'''
+    a=img.copy()
+    img3=np.zeros(img.shape,dtype=np.uint8)
+    #random_value=random.random()
+    per=translate/100*random_value
+    xm=int(per*img.shape[1])
+    if xm==0:
+        xm=1
+    x,y,w,h=b
+    w,h=w-x,h-y
+    if random_value>0.5:
+        img3[:,:-xm,:]=a[:,xm:,:]
+        x=x-xm
+        if (x+w)<0:
+            m=0
+        else:
+            m=x+w
+        if x<0:
+            x=0
+        coord=[x,y,m,y+h]
+        return img3,coord
+    else:
+        img3[:,xm:,:]=a[:,:-xm,:]
+        x=x+xm
+        if x>a.shape[1]:
+            x=a.shape[1]
+        if (x+w)>a.shape[1]:
+            m=a.shape[1]
+        else:
+            m=x+w
+        coord=[x,y,m,y+h]
+        return img3,coord
+
+
+def up_or_down(img,translate,random_value,b):
+    #translate
+    '''if random number >0.5 then down or else up'''
+    a=img.copy()
+    img3=np.zeros(img.shape,dtype=np.uint8)
+    per=translate/100*random_value
+    xm=int(per*img.shape[1])
+    if xm==0:
+        xm=1
+    x,y,w,h=b
+    w,h=w-x,h-y
+    if random_value>0.5:
+        img3[xm:,:,:]=a[:-xm,:,:]
+        y=y+xm
+        if (y+h)>a.shape[0]:
+            m=a.shape[0]
+        else:
+            m=y+h
+        if y<0:
+            y=0
+        coord=[x,y,x+w,m]
+        return img3,coord
+    else:
+        img3[:-xm,:,:]=a[xm:,:,:]
+        y=y-xm
+        if (y+h)<0:
+            m=0
+        else:
+            m=y+h
+        if y<0:
+            y=0
+        coord=[x,y,x+w,m]
+        return img3,coord
+
+def shear(img,sheared,random_value,b):
+    a=img.copy()
+    img3=np.zeros(a.shape,dtype=np.uint8)
+    per=sheared/100*random_value
+    xm=int(per*a.shape[1])
+    if xm==0:
+        sm=1
+    i1=a.shape[0]-1
+    alpha=xm/a.shape[0]
+    beta=0
+    j=0
+    while i1>0:
+        b1=a[i1,:,:]
+        c=np.zeros(b1.shape,dtype=np.uint8)
+        d=int(alpha*j)
+        if d==0:
+            d=1
+        c[d:,:]=b1[:-d,:]
+        img3[i1,:,:]=c
+        i1-=1
+        j+=1
+    x,y,w,h=b
+    w,h=w-x,h-y
+    d1=int(alpha*(a.shape[0]-y))
+    d2=int(alpha*(a.shape[0]-(y+h)))
+    coord=[d2+x,y,d1+x+w,y+h]
+    return img3,coord
+
+def rotate(img,angle,random_value,b):
+    a=img.copy()
+    a1=a.copy()
+    theta=angle*random_value
+    p1=theta/180*22/7
+    img3=imutils.rotate(a1,angle=theta)
+    x,y,w,h=b
+    w,h=w-x,h-y
+    #######for bounding box rotation
+    xcenter=int(x+w/2)
+    ycenter=int(y+h/2)
+    lt=np.array([[(x-xcenter),(-y+ycenter)]])
+    rt=np.array([[(x+w-xcenter),(-y+ycenter)]])
+    lb=np.array([[(x-xcenter),(-y-h+ycenter)]])
+    rb=np.array([[(x+w-xcenter),(-y-h+ycenter)]])
+    rotmat=np.array([[(math.cos(p1),-math.sin(p1)),(math.sin(p1),math.cos(p1))]])
+    lt1=np.dot(lt,rotmat)
+    rt1=np.dot(rt,rotmat)
+    lb1=np.dot(lb,rotmat)
+    rb1=np.dot(rb,rotmat)
+    ttt=(lt1,rt1,lb1,rb1)
+    smallx=lt1[0][0][0]
+    smally=lt1[0][0][1]
+    for i1 in ttt:
+        if i1[0][0][0]<smallx:
+            smallx=i1[0][0][0]
+        if i1[0][0][1]<smally:
+            smally=i1[0][0][1]
+    yc,xc,cc=np.multiply(a.shape,0.5)
+    xr=int(((xcenter-xc)*math.cos(p1))-(math.sin(p1)*(-ycenter+yc)))
+    yr=int(((xcenter-xc)*math.sin(p1))+(math.cos(p1)*(-ycenter+yc)))
+    if xr<0:
+        pt1=xc+xr
+    else:
+        pt1=xr+xc
+    if yr<0:
+        pt2=yc-yr
+    else:
+        pt2=yc-yr
+    coord=[int(pt1+smallx),int(pt2+smally),int(pt1-smallx),int(pt2-smally)]
+    return img3,coord
+
+def zoom_in(img,zoom,random_value,b):
+    a=img.copy()
+    a1=a.copy()
+    x,y,w,h=b
+    w,h=w-x,h-y
+    qw=zoom*random_value/100
+    imgnew=a1[int(qw*a.shape[0]):int(a.shape[0]-qw*a.shape[0]),
+             int(qw*a.shape[1]):int(a.shape[1]-qw*a.shape[1])]
+    x=int(x-qw*a.shape[1])
+    y=int(y-qw*a.shape[0])
+    w=x+w
+    h=y+h
+    img3=cv2.resize(imgnew,(a.shape[1],a.shape[0]))
+    x1=int(x/imgnew.shape[1]*a.shape[1])
+    y1=int(y/imgnew.shape[0]*a.shape[0])
+    w1=int(w/imgnew.shape[1]*a.shape[1])
+    h1=int(h/imgnew.shape[0]*a.shape[0])
+    coord=[x1,y1,w1,h1]
+    return img3,coord
+
+def display(img3,b):
+	cv2.rectangle(img3,(b[0],b[1]),(b[2],b[3]),(0,255,0),2)
+	cv2.resize(img3,(640,480))
+	cv2.imshow('kl',img3)
+
+	cv2.waitKey()
+
+
+def zoom_out(img,zoom,random_value,b):
+    a=img.copy()
+    x,y,w,h=b
+    w=w-x
+    h=h-y
+    if h==0:
+         h=1
+    if w==0:
+        w=1
+    val=w/h
+    qw=(zoom/100)*random_value
+    qwx=int(qw*img.shape[1])
+    qwy=int(qw*img.shape[0])
+    #print(qwx,qwy,a.shape)
+    if qwx==0:
+        qwx=1
+    if qwy==0:
+        qwy=1
+    img2=cv2.resize(a,(a.shape[1]-qwx*2,a.shape[0]-qwy*2))
+    img3=np.ones(img.shape,dtype=np.uint8)
+    img3[qwy:-qwy,qwx:-qwx,:]=img2
+    x=int((x/a.shape[1])*img2.shape[1])
+    y=int((y/a.shape[0])*img2.shape[0])
+    w=int((w/a.shape[1])*img2.shape[1])
+    h=int((h/a.shape[0])*img2.shape[0])
+    #print((qwx+x,qwy+y),(qwx+x+w,qwy+y+h),(a.shape[1]-qwx*2,a.shape[0]-qwy*2))
+    #print(img2.shape,x,y,w,h)
+    #cv2.rectangle(img3,(qwx+x,qwy+y),(qwx+x+w,qwy+y+h),(0,255,0),2)
+    #cv2.rectangle(a,(b[0],b[1]),(b[2],b[3]),(0,255,0),2)
+    #cv2.imshow('p',img3)
+    #cv2.imshow('po',a)
+    #cv2.waitKey(1)
+    cd=[qwx+x,qwy+y,qwx+x+w,qwy+y+h]
+    return img3,cd
+
+out=open('./augment.csv','w')
+out.write('name,iw,ih,x1,y1,x2,y2,class\n')
+
+f=pd.read_csv('./d<2.csv')
+name1='none'
+ac=0
+for i in f.iterrows():
+	ac=0
+	name,x1,y1,x2,y2,cl=i[1]
+	
+	if name1!=name:
+		rv=random.random()
+		rv1=random.random()
+		rv2=random.random()
+		rv3=random.random()
+		rv4=random.random()
+		rv5=random.random()
+		rv6=random.random()
+		name1=name
+		img=cv2.imread(name)
+		ac=1	
+	b=[x1,y1,x2,y2]
+	#zoom=30
+	translate,sheared,angle,zoom=20,20,60,20
+	img3,b=zoom_out(img,zoom,rv,b)
+	img3,b=up_to_down(img3,rv1,b)
+	#display(img3,b)
+	img3,b=left_to_right(img3,rv2,b)
+	#display(img3,b)
+	img3,b=up_or_down(img3,translate,rv3,b)
+	#display(img3,b)
+	img3,b=left_or_right(img3,translate,rv4,b)
+	#display(img3,b)
+	img3,b=shear(img3,sheared,rv5,b)
+	#display(img3,b)
+	img3,b=rotate(img3,angle,rv6,b)
+	display(img3,b)
+	#print(rv,rv1,rv2,rv3,rv4,rv5,rv6)
+	#img3,b=zoom_in(img3,zoom,rv,b)
+	cd=b
+	ih,iw,ic=img3.shape
+	if b[0]<0 or b[1]<0 or b[2]<0 or b[3]<0 or b[2]>iw or b[3]>ih:
+		continue
+	if name1!=name:
+		name='./final_images/'+name.split('/')[-1]
+		cv2.imwrite(name,img3)
+	name='./final_images/'+name.split('/')[-1]
+	out.write('{},{},{},{},{},{},{},{}\n'.format(name,iw,ih,cd[0],cd[1],cd[2],cd[3],cl))
+	#break
--- build_records.py
+++ build_records.py
@@ -11,12 +11,34 @@ from sklearn.model_selection import train_test_split
 from PIL import Image
 import tensorflow as tf
 import os
+import pandas as pd
 
 
+def int64_feature(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def int64_list_feature(value):
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+def bytes_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def bytes_list_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+
+
+def float_list_feature(value):
+  return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+CLASSES={'Pedestrian':1,'Twowheeler':2,'Fourwheeler':3,'Truck':4,'Bus':5,'Other':6}
 def main(_):
     f = open(config.CLASSES_FILE, "w")
 
-    for (k, v) in config.CLASSES.items():
+    for (k, v) in CLASSES.items():
         item = ("item {\n"
                 "\tid: " + str(v) + "\n"
                 "\tname: '" + k + "'\n"
@@ -27,11 +49,11 @@ def main(_):
     # initialize data dictionary to map each image filename to all bounding boxes associated with image,
     # load contents of annotation file
     D = {}
-    rows = open(config.ANNOT_PATH).read().strip().split("\n")
+    rows = pd.read_csv('./augment.csv')
 
-    for row in rows[1:]:
-        row = row.split(",")
-        (imagePath, width, height, label, startX, startY, endX, endY) = row
+    for row in rows.iterrows():
+        row = row[1]
+        imagePath, width, height,startX, startY, endX, endY,label = row
         (startX, startY) = (float(startX), float(startY))
         (endX, endY) = (float(endX), float(endY))
 
@@ -41,18 +63,18 @@ def main(_):
         # building path to input image, grabbing other bounding boxes and labels associated with image
         # path, labels and bounding box lists respectively
         #imagePath='/images'+'/'+imagePath
-        p = os.path.sep.join([config.BASE_PATH, imagePath])
+        p = os.path.sep.join(['/home/ubuntu/vd', imagePath])
         p=imagePath
         b = D.get(p, [])
 
         b.append((label,width,height, (startX, startY, endX, endY)))
         D[p] = b
 
-    (trainKeys, testKeys) = train_test_split(list(D.keys()), test_size=config.TEST_SIZE, random_state=42)
+    (trainKeys, testKeys) = train_test_split(list(D.keys()), test_size=0.2, random_state=42)
 
     datasets = [
-        ("train", trainKeys, config.TRAIN_RECORD),
-        ("test", testKeys, config.TEST_RECORD)]
+        ("train", trainKeys, './records/train.tfrecord'),
+        ("test", testKeys, './records/test.tfrecord')]
 
     for (dType, keys, outputPath) in datasets:
         print("[UPDATE] processing '{}'...".format(dType))
@@ -60,21 +82,21 @@ def main(_):
         total = 0
         for k in keys:
 
-            encoded = tf.io.gfile.GFile(k.split("/")[-1], "rb").read()
-            encoded = bytes(encoded)
+            encoded = tf.io.gfile.GFile(k).read()
+            #encoded = encoded.tobytes()
             
-            pilImage = Image.open(k)
+            pilImage = Image.open(io.BytesIO(encoded))
             (w, h) = pilImage.size[:2]
             
             filename = k.split(os.path.sep)[-1]
             encoding = filename[filename.rfind(".") + 1:]
             
-            tfAnnot = TFAnnotation()
-            tfAnnot.image = encoded
-            tfAnnot.encoding = encoding
-            tfAnnot.filename = filename
-            tfAnnot.width = w
-            tfAnnot.height = h
+            #tfAnnot = TFAnnotation()
+            image = bytes_feature(encoded.encode('utf8')
+            encoding = bytes_feature(encoding)
+            filename = bytes_feature(filename)
+            width = w
+            height = h
 
             for (label, w,h,(startX, startY, endX, endY)) in D[k]:
 
@@ -83,18 +105,30 @@ def main(_):
                 yMin = startY / h
                 yMax = endY / h
 
-                tfAnnot.xMins.append(xMin)
-                tfAnnot.xMaxs.append(xMax)
-                tfAnnot.yMins.append(yMin)
-                tfAnnot.yMaxs.append(yMax)
-                tfAnnot.textLabels.append(label.encode("utf8"))
-                tfAnnot.classes.append(config.CLASSES[label])
-                tfAnnot.difficult.append(0)
+                xMins.append(xMin)
+                xMaxs.append(xMax)
+                yMins.append(yMin)
+                yMaxs.append(yMax)
+                textLabels.append(label.encode("utf8"))
+                classes.append(CLASSES[label])
+                #tfAnnot.difficult.append(0)
 
                 total += 1
 
-            features = tf.train.Features(feature=tfAnnot.build())
-            example = tf.train.Example(features=features)
+            #features = tf.train.Features(feature=tfAnnot.build())
+            example = tf.train.Example(tf.train.Features(feature={
+           'image/height': int64_feature(height),
+           'image/width': int64_feature(width),
+           'image/filename': bytes_feature(filename),
+           'image/source_id': bytes_feature(filename),
+           'image/encoded': bytes_feature(encoded_jpg),
+           'image/format': bytes_feature(image_format),
+           'image/object/bbox/xmin': float_list_feature(xMins),
+           'image/object/bbox/xmax': float_list_feature(xMaxs),
+           'image/object/bbox/ymin': float_list_feature(yMins),
+           'image/object/bbox/ymax': float_list_feature(yMaxs),
+           'image/object/class/text': bytes_list_feature(textLabels),
+           'image/object/class/label': int64_list_feature(classes), }))
 
             writer.write(example.SerializeToString())
 
