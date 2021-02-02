b6dd7d84a43460ec8f869a32df41360d7e220c25
2vin2vin <54850096+2vin2vin@users.noreply.github.com>
2021-02-02 20:12:58 +0530

    Delete AirodynCV directory

--- AirodynCV/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-#  Copyright (c) 2020.
-#  Airodyn Systems Private Limited
-
-# ______________________________________
-#   File Name:          variableCNN_train.py
-#   Author:             Aviral Sharma
-# ______________________________________
